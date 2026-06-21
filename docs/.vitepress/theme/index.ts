d9003741e5495f2ffa1b1a6b8f1df0ad1eac775f
Kyle McCullough <director@thebarc.ca>
2026-06-21 17:36:54 -0300

    Adopt Mere docs theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,14 @@
-import DefaultTheme from 'vitepress/theme'
-import './custom.css'
+import { createMereProductDocsTheme } from './mere/index.js'
 
-export default DefaultTheme
+export default createMereProductDocsTheme({
+  productName: 'mere.run',
+  productDomain: 'docs.mere.run',
+  docsUrl: 'https://docs.mere.run/',
+  productHref: 'https://mere.run',
+  corePrefix: 'Local-first inference on Apple Silicon with public CLI and runtime docs.',
+  guideHref: '/getting-started',
+  architectureHref: '/architecture',
+  operationsHref: '/development-workflow',
+  referenceHref: '/cli',
+  cliHref: '/cli'
+})
