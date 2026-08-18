67a5757433e8560041f2a71d5ea0a871e0713d39
Kyle McCullough <director@thebarc.ca>
2026-08-18 11:22:31 -0300

    Release v0.41.0

--- Tests/MereRunCLITests/CLIModelStoreBootstrapTests.swift
+++ Tests/MereRunCLITests/CLIModelStoreBootstrapTests.swift
@@ -130,7 +130,7 @@ final class CLIModelStoreBootstrapTests: XCTestCase {
     }
 
     func testMereRunCLIExposesReleaseVersion() {
-        XCTAssertEqual(MereRunCLIVersion.current, "0.40.1")
+        XCTAssertEqual(MereRunCLIVersion.current, "0.41.0")
         XCTAssertEqual(MereRunCLI.configuration.version, MereRunCLIVersion.current)
     }
 
